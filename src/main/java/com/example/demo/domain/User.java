15bb4d5f3fe3a6f7db05dcff87c058690ac16488
angela <peekaboo32@naver.com>
2023-03-08 17:32:11 +0900

    User 모델 생성

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,5 +1,37 @@
 package com.example.demo.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class User {
 
+	private Integer id;
+	private String username;
+	private String password;
+	private String name;
+	private String website;
+	private String bio;
+	private String email;
+	private String phone;
+	private String gender;
+	private String prifileImageUrl;
+	private String role;
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist
+	public void createDate() {
+		this.createDate= LocalDateTime.now();
+	}
+	
 }
