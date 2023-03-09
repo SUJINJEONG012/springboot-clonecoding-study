4353be1af9c9d96003606e9bd96641e95aed0820
angela <peekaboo32@naver.com>
2023-03-09 13:57:07 +0900

    User 를 toEntity 객체를 담아서 회원가입 기능

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,12 +8,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.User;
 import com.example.demo.dto.SignupDto;
+import com.example.demo.service.AuthService;
 
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Controller
 public class AuthController {
 	
 	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+	
+	
+	//@Autowired
+	private final  AuthService authService;
+	
 	// 로그인 페이지로 이동
 	@GetMapping("/auth/signin")
 	public String singinForm() {
@@ -33,7 +41,9 @@ public class AuthController {
 		log.info("@@@ log확인 ::"+ signupDto.toString());
 		
 		User user = signupDto.toEntity();
-		log.info(user.toString());
+		//log.info(user.toString());
+		
+		authService.회원가입(user);
 		 
 		return "auth/signin"; //회원가입이 되면 로그인페이지로 이동하게끔
 	}
--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -2,10 +2,21 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class AuthService {
+	
+	private final UserRepository userRepository;
 
-	public void 회원가입() {
+	public User 회원가입(User user) {
 		//회원가입 진행
+		User userEntity = userRepository.save(user);
+		
+		return userEntity;
 	}
 }
