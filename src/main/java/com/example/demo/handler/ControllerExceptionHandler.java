66478e56d8a057951a79775a79b7f8e16499a4d8
angela <peekaboo32@naver.com>
2023-03-10 09:26:16 +0900

    error.getDefaultMessage()을 리턴값은 map이라서 Exception을 따로 만들어준다.

--- src/main/java/com/example/demo/handler/ControllerExceptionHandler.java
+++ src/main/java/com/example/demo/handler/ControllerExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.example.demo.handler;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //데이터를 응답 할 수 잇게
 @ControllerAdvice //모든 Exception을 여기로 받아준다.
 public class ControllerExceptionHandler {
 
+	@ExceptionHandler(RuntimeException.class)
+	public String validationException(RuntimeException e) {
+		return e.getMessage();
+	}
 	
 }
---
+++ src/main/java/com/example/demo/handler/ex/CustomValidationException.java
@@ -0,0 +1,12 @@
+package com.example.demo.handler.ex;
+
+public class CustomValidationException extends RuntimeException {
+
+
+    // 객체를 구분하기위 해 시리얼 넘버를 넣어준느것
+	 //jvw을 위해 걸어주는 것
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
