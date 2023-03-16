35f098b7d822ab87a7596b504cb7510d3ef849bc
angela <peekaboo32@naver.com>
2023-03-16 10:41:02 +0900

    로그인 후 페이지 구독한 사람 나오도록 설정하기위해 구독 모델  만들기

--- photogram-master/src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cos.photogramstart.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-    // JPA Query Method
-    User findByUsername(String username);
-}
--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,21 +1,16 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.User;
 import com.example.demo.dto.SignupDto;
-import com.example.demo.handler.ex.CustomValidationException;
 import com.example.demo.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -43,26 +38,13 @@ public class AuthController {
 	}
 	
 	
-	//회원가입기능
+//회원가입
 	@PostMapping("/auth/signup")
 	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
-		
-		if(bindingResult.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-			
-			for (FieldError error : bindingResult.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-				System.out.println(error.getDefaultMessage());	
-			}
-		   throw new CustomValidationException("유효성검사를 실패하였습니다. ", errorMap);
-		}else {
-			//log.info("@@@ log확인 ::"+ signupDto.toString());
-			User user = signupDto.toEntity();
-			log.info(user.toString());
-			authService.회원가입(user); 
-			return "auth/signin"; //회원가입이 되면 로그인페이지로 이동하게끔
-		}
-		
+		User user = signupDto.toEntity();
+		authService.회원가입(user);
+		return "/auth/signin";
+	
 	}
 	
 	
